9952b9497f04e9b6d1203851e367d2f378856c6b
JonasJ-ap <rushanj@andrew.cmu.edu>
2022-10-01 23:21:09 -0400

    fix format

--- quantum_test.py
+++ quantum_test.py
@@ -1,12 +1,12 @@
 import pytest
-from quantum import *
+import quantum
 import numpy as np
 
 
-def unique_circuit(arr):
-    return arr == int("0001", 2)
+def unique_circuit(bits):
+    return bits == int("0001", 2)
 
 
 def test_unique_sat_solver():
-    qs = unique_sat_solver(unique_circuit, 4, 1)
+    qs = quantum.unique_sat_solver(unique_circuit, 4, 1)
     assert qs.get_amplitudes()[1] == 2.75
